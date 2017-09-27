bc48d01a917fc0134cfd294cf09645a3a4aaa6a2
vkhLeslie <vkh1990@163.com>
2017-09-27 08:27:18 +0800

    Update mutations.js

--- src/store/user/mutations.js
+++ src/store/user/mutations.js
@@ -4,11 +4,11 @@
 const mutations = {
 
     // 获取用户数据
-    'SET_UID':( state,list ) => {
-     state.user.uid = state.user.uid;
+    'SET_UID':( state,uid ) => {
+     state.uid = uid;
     },
-     'SET_SSO_TOKEN':( state,list ) => {
-        state.user.token = state.user.token;
+     'SET_SSO_TOKEN':( state,token ) => {
+        state.token = token;
     },
       
 };
